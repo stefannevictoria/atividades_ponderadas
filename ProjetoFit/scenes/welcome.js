76e9d033b4a17da3e4d5f7dabcf45eea5672b2ec
Stefanne Victória Soares <stefanne.soares@sou.inteli.edu.br>
2025-03-07 15:20:07 -0300

    adicionando botões

--- ProjetoFit/scenes/welcome.js
+++ ProjetoFit/scenes/welcome.js
@@ -11,20 +11,58 @@ export class WelcomeScene extends Phaser.Scene {
         this.load.image('jeniffer', 'assets/jeniffer.png');
         this.load.image('titulo', 'assets/textos/titulo.png');
         this.load.image('inicio', 'assets/textos/inicio.png');
+        this.load.image('fundoSett', 'assets/fundoSettings.png');
+        this.load.image('back', 'assets/botoes/voltar.png');
     }
 
     create () {
         this.larguraJogo = this.sys.game.config.width;  // Usando a configuração do Phaser
         this.alturaJogo = this.sys.game.config.height;  // Usando a configuração do Phaser
 
-        this.add.image(this.larguraJogo/2, this.alturaJogo/2, 'fundo');
+        this.fundo = this.add.image(this.larguraJogo/2, this.alturaJogo/2, 'fundo');
         this.add.image(this.larguraJogo/2, 100, 'titulo');
         this.add.image(this.larguraJogo/2, 190, 'inicio').setScale(0.6); 
         this.add.image(this.larguraJogo/2, 350, 'jeniffer').setScale(0.8);
 
-        this.add.image(550, 560, 'settings').setScale(0.8);
+        this.settings = this.add.image(this.larguraJogo/2, this.alturaJogo/2, 'fundoSett').setScale(0.7).setVisible(false);
+
+        this.botaoVoltar = this.add.image(700, 100, 'back').setScale(0.6).setVisible(false);
+
+        this.botaoConfig = this.add.image(500, 560, "settings").setScale(0.8).setInteractive();
 
         this.botaoJogar = this.add.image(300, 560, "play").setScale(0.8).setInteractive();
+
+        this.botaoConfig.on("pointerover", () => {
+            this.input.setDefaultCursor("pointer");
+        });
+        
+        this.botaoConfig.on("pointerout", () => {
+            this.input.setDefaultCursor("default");
+        });
+
+        this.botaoConfig.on("pointerdown", () => {
+            this.settings.setVisible(true);
+            this.botaoConfig.setVisible(false);
+            this.botaoJogar.setVisible(false);
+            this.fundo.setVisible(false);
+            this.botaoVoltar.setVisible(true);
+        })
+
+        this.botaoVoltar.on("pointerover", () => {
+            this.input.setDefaultCursor("pointer");
+        });
+        
+        this.botaoVoltar.on("pointerout", () => {
+            this.input.setDefaultCursor("default");
+        });
+
+        this.botaoVoltar.on("pointerdown", () => {
+            this.settings.setVisible(false);
+            this.botaoConfig.setVisible(true);
+            this.botaoJogar.setVisible(true);
+            this.fundo.setVisible(true);
+            
+        })
 
         this.botaoJogar.on("pointerover", () => {
             this.input.setDefaultCursor("pointer");
@@ -35,8 +73,10 @@ export class WelcomeScene extends Phaser.Scene {
         });
 
         this.botaoJogar.on("pointerdown", () => {
-            this.scene.start("GameScene")
+            this.scene.start("GameScene");
         })
+
+        
     }
 
     update() {
